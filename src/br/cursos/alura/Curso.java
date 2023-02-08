424681965f4739db991c28c0d1a031a9451a6605
Raphael Anizio <raphaelanizio01@gmail.com>
2023-02-08 20:38:43 -0300

    Aprendendo a fazer relação entre duas classes usando forma defensiva

--- src/br/cursos/alura/Curso.java
+++ src/br/cursos/alura/Curso.java
@@ -2,6 +2,7 @@ package br.cursos.alura;
 // Aqui vamos trabalhar com relação de classe usando as coleções
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,6 +11,7 @@ public class Curso {
     private String instrutor;
     // vamos fazer um relacionamento entre a classe Curso com a classe Aula
     // na classe Curso vai conter quais aulas pertencem aquele curso
+    // inicializando a lista de aulas vazia
     private List<Aula> aulas = new LinkedList<Aula>();
 
     public Curso(String nome, String instrutor) {
@@ -28,6 +30,17 @@ public class Curso {
     // feito o relacionamento entre as classes Curso e Aula agora temos acesso as aulas do curso
     // get de aulas para poder ter acesso as aulas fora da classe
     public List<Aula> getAulas() {
-        return aulas;
+        // vamos usar uma forma defensiva para não deixar adicionar nehuma aula de outra jeito que agente estipulou
+        // ou seja para adicionar uma aula tem que usar o metodo que agente criou
+        // vamos usar Collections.unmodifiableList que não vai deixar nenhuma aula ser adicionado de outra forma
+        return Collections.unmodifiableList(aulas);
+
+        // Collections.unmodifiableList vai devolver uma lista parecida com a que temos porém so que vai servir para leitura
+        // ou seja todos os mettodos que usarmos para modificar a lista ele não vai deixar ou remover adicionar etc...
+    }
+
+    // metodo que vai ser responsavel por somente adicionar as aulas
+    public void adiciona(Aula aula) {
+        this.aulas.add(aula);
     }
 }
---
+++ src/br/cursos/alura/TestaCurso.java
@@ -0,0 +1,20 @@
+package br.cursos.alura;
+
+public class TestaCurso {
+    public static void main(String[] args) {
+        // criando curso
+        Curso javaColecoes = new Curso("Dominando as coleções Java", "Paulo Silveira");
+        // adicionando uma nova aula dentro do curso, vamos usar o metodo adiciona que criamos
+        javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 20));
+        javaColecoes.adiciona(new Aula("Utilizando estrutura de dados", 10));
+        javaColecoes.adiciona(new Aula("Criando uma aula", 8));
+
+        // usamos a forma defensiva que não vai permitir que a aula seja adicionado dessa forma a aula so sera adicionado
+        // usando o metodo que agente criou na classe
+        //javaColecoes.getAulas().add(new Aula("Não vai funcionar", 5));
+
+        // pegando a aula adicionada e imprimindo
+        System.out.println(javaColecoes.getAulas());
+
+    }
+}
